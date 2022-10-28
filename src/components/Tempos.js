55b9bfdf17ac2b85200fd120595208278105666a
Leonardo Niekiforuk <leonardoniekiforuk@hotmail.com>
2022-10-28 11:44:59 -0300

    Element form input functional

--- src/components/Tempos.js
+++ src/components/Tempos.js
@@ -3,29 +3,39 @@ import Elemento from './Elemento';
 import Divider from './Divider';
 
 const Tempos = () => {
-  const[show,setShow] = useState([true])
+  // const[show,setShow] = useState([true])
   const[times,setTimes] = useState(1);
   const handleClick=()=>{
     setTimes(times+1)
-    setShow(show => [...show, true])
-    setShow(show[times-1] = false)
+    // const temp = show.slice();
+    // temp.push(true);
+    // temp[times-1] = false;
+    // setShow(temp)
   }
+
   const handleChange = ()=>{
     console.log()
   }
+
+  // const showAll=()=>{
+  //   const temp = show.map((e) => true)
+  //   setShow(temp)
+  // }
 
   return (
     <div className="form-holder">
     <h1 className="title">Elementos</h1>
       {[...Array(times)].map((e, i) =>
-      show ? (
-      <>
-        <Divider>{`Elemento ${i+1}`}</Divider>
-        <Elemento key={i} onChange={handleChange}/>
-      </>
-      ):(
-      <Divider>{`Elemento ${i+1}`}</Divider>
-      ))}
+      // show[i] ? (
+        <>
+          <Divider>{`Elemento ${i+1}`}</Divider>
+          <Elemento key={i} onChange={handleChange}/>
+        </>
+      // ):(
+      // <Divider>{`Elemento ${i+1}`}</Divider>
+      // )
+      )}
+      {/* <button className="submit-button" onClick={showAll}>show All</button> */}
       <button className="submit-button" onClick={handleClick}>Novo Elemento</button>
     </div>
   );
