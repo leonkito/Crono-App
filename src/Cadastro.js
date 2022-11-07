2612aec22c081e475f061f14a916ab65bbd4adb6
Leonardo Niekiforuk <leonardoniekiforuk@hotmail.com>
2022-11-07 16:58:42 -0300

    one component to rule them all (state)

--- src/Cadastro.js
+++ src/Cadastro.js
@@ -15,8 +15,7 @@ const Cadastro = () => {
     data:'',
     observacao:'',
   })
-  const[element,setElement] = useState([[{
-    id:0, 
+  const[element,setElement] = useState([{
     elemento:'',
     ritmo:100,
     frequencia:1,
@@ -24,7 +23,8 @@ const Cadastro = () => {
     tempoControle:0,
     tempoNormal:0,
     tempoBase:0,
-  }],])
+    time:[""],
+  }])
   // const[{concessao, tempoNormal, tempoBasico}] = useState({
   //   concessao:'',
   //   tempoNormal:'',
@@ -32,10 +32,9 @@ const Cadastro = () => {
   // })
 
   const[times,setTimes] = useState(1);
-  const handleClick=()=>{
+  const addElement =()=>{
     setTimes(times+1)
-    const tempElement = [{
-      id:times,
+    let newElement = {
       elemento:'',
       ritmo:100,
       frequencia:1,
@@ -43,26 +42,49 @@ const Cadastro = () => {
       tempoControle:0,
       tempoNormal:0,
       tempoBase:0,
-      tempoCiclos:[''],
-    }]
-  setElement(current => [...current, tempElement]);
-    console.log(element[0][0])
+      time:[""],
+    }
+    setElement ([...element,newElement])
+    console.log(element[0])
   }
 
   const handleChange = ({target:{name, value}})=>{
     setState(prevState =>({...prevState, [name]:value}));
   }
 
-  const handleElement = (e)=>{
-    // setElement(prevState =>({...prevState, element[0][0].[name]:value}));
+  const handleElementChange = (index, event)=>{
+    let data = [...element];
+    data[index][event.target.name] = event.target.value;
+    setElement(data)
+  }
 
-    let temp = element[0][0]
-    temp[e.target.name] = e.target.value;
-    setElement(prevState =>({...prevState,temp}))
-  };
-
+  const handleTime = (index,event,i) =>{
+    let data = [...element];
+    data[index][event.target.name][i] = event.target.value;
+    setElement(data)
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(element[0])
+  }
+
+  // useEffect(function() {
+  //   const id = setInterval(function log() {
+  //     const sum = Object.values(time).reduce((accumulator, value) => {
+  //       return accumulator + Number(value);
+  //     }, 0);
+  //     const tm = Math.round((sum/(Object.values(time).length)) * 100) / 100;
+  //     const tn = Math.round((tm*(element.ritmo/100)) * 100) / 100;
+  //     const tb = Math.round(((tn/element.frequencia)*(1+(element.fadiga/100))) * 100) / 100;
+  //     setElement(prevState =>({...prevState,element.tempoControle:Number(tm), element.tempoNormal:Number(tn), element.tempoBase:Number(tb)}))
+  //   }, 300);
+  //   return function() {
+  //     clearInterval(id);
+  //   }
+  // }, [time, element.fadiga, element.ritmo, element.frequencia]);
+  
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
     // const timeRef = ref(databaseConfig, `tempos/${codigo}/${operacao}/${revisao}`)
     // set(timeRef,{
     //   cronoanalista : cronoanalista,
@@ -70,23 +92,34 @@ const Cadastro = () => {
     //   observacao:observacao
     // })
     // TODO add on edit mode to change in database only what has been changed in the forms, cause set changes all the reference if nothing else is especified 
-  }
+  // }
+  const[ciclos,setCiclos] = useState(10);
 
+  const handleCiclos = (e) => {
+    e.preventDefault();
+    setCiclos(ciclos+10)
+  }
   return (
     <div className="main">
-    <Informacoes handleChange={handleChange} state={state}/>
+      <Informacoes handleChange={handleChange} state={state}/>
       <div className="form-holder">
-          <h1 className="title">Elementos</h1>
-          <form>
-            {[...Array(times)].map((e, i) =>
-              <>
-                <Divider>{`Elemento ${i+1}`}</Divider>
-                <Elemento handleElement={handleElement} key={i} element={element[i][0]}/>
-              </>
-            )}
-            </form>
-            <button className="submit-button medium-btn" onClick={handleClick}>Adicionar Elemento</button>
-            <button className="submit-button small-btn" onClick={handleSubmit}>Submit</button>
+        <h1 className="title">Elementos</h1>
+        <form>
+          {element.map((elements,index)=>
+          <div key={`d${index}`}>
+            <Divider>{`Elemento ${index+1}`}</Divider>
+            <Elemento element={elements} handleElementChange={event => handleElementChange(index, event)}/>
+            <label>Ciclos:
+            <div className="input-time-holder">
+              {[...Array(ciclos)].map((e, i) =><input key={`t${i}`} placeholder={`t${i+1}`} name="time" value={elements.time[i]|| ""} onChange={event => handleTime(index, event,i)} className="input-time-form"></input>)}
+            </div>
+            <button className="submit-button small-btn" onClick={handleCiclos}>+ Tempos</button>
+            </label>    
+          </div>
+          )}
+        </form>
+        <button className="submit-button medium-btn" onClick={addElement}>Adicionar Elemento</button>
+        <button className="submit-button medium-btn" onClick={handleSubmit}>submit</button>
       </div>
     </div>
   );
--- src/components/Create/Elemento.js
+++ src/components/Create/Elemento.js
@@ -1,30 +1,6 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 
-const Elemento = ({handleElement, element}) => {
-  // const[show,setShow] = useState(true);
-  const[time,setTime] = useState([""]);
-  const[ciclos,setCiclos] = useState(10);
-
-  const handleTime = ({target:{name,value}})=>{
-      setTime(prevState =>({...prevState,[name]:value}));      
-  }
-
-  useEffect(function() {
-    const id = setInterval(function log() {
-      const sum = Object.values(time).reduce((accumulator, value) => {
-        return accumulator + Number(value);
-      }, 0);
-      const tm = Number(Math.round((sum/(Object.values(time).length)) * 100) / 100);
-      const tn = Number(Math.round((tm*(element.ritmo/100)) * 100) / 100);
-      const tb = Math.round(((tn/element.frequencia)*(1+(element.fadiga/100))) * 100) / 100;
-      // setState(prevState =>({...prevState,tempoControle:Number(tm), tempoNormal:Number(tn), tempoBase:Number(tb),tempoCiclos:time}))
-    }, 300);
-    return function() {
-      clearInterval(id);
-    }
-  }, [time, element.fadiga, element.ritmo, element.frequencia]);
-  // useEffect roda apenas quando o objeto ou variavel muda, conforme [time]
-
+const Elemento = ({handleElementChange, element}) => {
   return (
     <>
       <div className="input-holder">
@@ -36,16 +12,11 @@ const Elemento = ({handleElement, element}) => {
           value={element.elemento}
           className="input-form"
           name="elemento"
-          onChange={handleElement}
+          onChange={handleElementChange}
           />
         </label>
       </div>
-      <label>Ciclos:
-      <div className="input-time-holder">
-        {[...Array(ciclos)].map((e, i) =><input placeholder={`t${i+1}`} name={i} key={i} type="number" value={time[i] || ''} onChange={handleTime} className="input-time-form"></input>)}
-      </div>
-      <button className="submit-button small-btn" onClick={() =>{setCiclos(ciclos+10)}}>+ Tempos</button>
-      </label>      
+  
       <div className="input-times-holder">
         <label> 
           Ritmo(em %): 
@@ -55,7 +26,7 @@ const Elemento = ({handleElement, element}) => {
           value={element.ritmo}
           className="input-percentage-form"
           name="ritmo"
-          onChange={handleElement}
+          onChange={handleElementChange}
           />
         </label>
         <label> 
@@ -66,7 +37,7 @@ const Elemento = ({handleElement, element}) => {
           value={element.frequencia}
           className="input-percentage-form"
           name="frequencia"
-          onChange={handleElement}
+          onChange={handleElementChange}
           />
         </label>
         <label> 
@@ -77,7 +48,7 @@ const Elemento = ({handleElement, element}) => {
             value={element.fadiga}
             className="input-percentage-form"
             name="fadiga"
-            onChange={handleElement}
+            onChange={handleElementChange}
           />
         </label>
         <label> 
